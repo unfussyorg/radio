330501fe8d18b7d737f9c7376b4ab4681d466f3c
Justin Turner <justin@unfussy.org>
2025-03-17 23:44:48 -0700

    wifi fix

--- include/config.h
+++ include/config.h
@@ -2,10 +2,5 @@
 #define SSID "Mishima"
 #define SSID_PASS "cruiser.own.hemlock"
 
-// house eero 5 ghz low
-#define ACCESS_POINT 0x84, 0x70, 0xd7, 0x56, 0x41, 0x46
-
-// shop eero 2.4 ghz
-/*
-#define ACCESS_POINT 0xC4, 0xF1, 0x74, 0x47, 0xe8, 0x27
-*/
+// house eero 2.4 ghz - ESP32 does not support 5 ghz
+#define ACCESS_POINT 0x84, 0x70, 0xd7, 0x56, 0x41, 0x47
