7f0540a687e9dde0f0a3f864d4fb75a4f712536a
Justin Turner <justin@unfussy.org>
2025-03-17 23:24:09 -0700

    delete example

--- include/config_EXAMPLE.h
+++
@@ -1,4 +0,0 @@
-// wifi credentials
-#define SSID "YourSSID"
-#define SSID_PASS "YourPassword"
-#define ACCESS_POINT 0x1A, 0x1A, 0x1A, 0x1A, 0x1A, 0x1A // If you want Arduino to connect to a specific router, enter BSSID
